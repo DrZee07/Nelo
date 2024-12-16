d66677124f9dd44a3b0a59ceb8895890bc679539
DrZee07 <Drzee12051205@gmail.com>
2024-12-17 04:01:27 +0500

    Dr Zee

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-800 shadow-md mt-auto">
+      <div className="container mx-auto px-4 py-3 text-center text-gray-600 dark:text-gray-300">
+        © 2024 NelsonBot. All rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
+
